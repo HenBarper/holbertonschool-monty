7d74dabf89b347701d7db21ab8a46d9ea2d11785
HenBarper <Benjaminitonio@gmail.com>
2023-03-30 10:47:46 -0700

    Added free_stack prototype.

--- monty.h
+++ monty.h
@@ -54,5 +54,6 @@ void pop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
 
+void free_stack(stack_t **stack);
 
 #endif
